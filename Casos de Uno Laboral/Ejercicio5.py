9e200cb8dd65aba301b4b63fbbdfbda43462e47c
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2023-12-22 19:25:26 -0500

    Subiendo Ejercicio5. Uso Laboral

---
+++ Casos de Uno Laboral/Ejercicio5.py
@@ -0,0 +1,29 @@
+print("")
+print("5. Software de restaurante en donde dependiendo de la comida que pida el cliente,se le da el precio y la descripción de lo que pidió.")
+print("")
+
+# (SIN TERMINAR)  
+
+try: 
+    selecPedido = 0
+    while selecPedido<5:
+        class menu:
+            print("Seleccion el Plato")
+            print("1. Hamburguesa Barbecur por $17.990")
+            print("2. Hamburguesa Grande Triple Queso por $24.990")
+            print("3. HamburguesaGrande Doble Cuarto De Libra por 27.990")
+            print("4. Pidela en combo con papas y Gaseosa por $5.000")
+            print("5. Pedir Menu y Salir")
+        selecPedido = int(input("Ingresa una opcion"))
+        print(selecPedido)  
+
+        # (SIN TERMINAR)  
+
+    
+        
+
+       
+
+
+except:
+    print("Eso no es un numero")
